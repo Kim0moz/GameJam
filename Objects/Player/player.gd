17933fc3b92bf864467585e06221ac80b4c38f3f
Rene Elias <reneelias03@gmail.com>
2024-11-05 09:49:22 -0800

    Import Player scene, update Input Map, move main camera onto player

---
+++ Objects/Player/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+@export var cameraController : Node3D
+@export var cameraLerpSpeed : float = .1
+@export var camerTurnSpeed : float = 2
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("movement_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	if Input.is_key_pressed(KEY_LEFT):
+		rotate_y(deg_to_rad(camerTurnSpeed))
+	elif Input.is_key_pressed(KEY_RIGHT):
+		rotate_y(deg_to_rad(-camerTurnSpeed))
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("movement_left", "movement_right", "movement_forward", "movement_backward")
+	input_dir.rotated(rotation.y)
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+	cameraController.position = position
+	cameraController.rotation.y = rotation.y

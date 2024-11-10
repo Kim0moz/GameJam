31d4fb765162d570953bf86a035fda73a0266455
Kimomo <d.s.zampathas@gmail.com>
2024-11-10 13:23:27 -0800

    Added Info bar

--- Sprites/DeliveryInfo.gd
+++ Sprites/DeliveryInfo.gd
@@ -1,6 +1,14 @@
+@tool
 extends HBoxContainer
 
 @export var Tiles : Array[Texture2D]
+@onready var Icon : TextureRect = $Icon/TextureRect
+@export_range(0,4) var TileSelected : int :
+	set(val):
+		Icon.texture = Tiles[val]
+		TileSelected = val
+	get:
+		return TileSelected
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:

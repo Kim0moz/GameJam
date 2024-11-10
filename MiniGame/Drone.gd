d1481548fed5f6051da9cf611ee04b9079e48ffe
Rene Elias <reneelias03@gmail.com>
2024-11-09 19:21:01 -0800

    Add ring particles to drone

--- MiniGame/Drone.gd
+++ MiniGame/Drone.gd
@@ -1,1 +1,31 @@
 extends Controller2D
+
+@export var ringParticles : CPUParticles2D
+## Offset from the drone body from which particles emit
+@export var ringParticlesOffset : float = -4
+## How much to offset the particle angles by -- CURRENTLY DOESN'T DO ANYTHING
+@export var particleAngleOffset : float = 270
+
+func _physics_process(delta):
+    super(delta)
+
+    if velocity.length() != 0:
+        ringParticles.emitting = true
+        var normVel = velocity.normalized()
+        ringParticles.direction = normVel * -1
+        ringParticles.position = normVel * ringParticlesOffset
+        # var particleAngle = rad_to_deg(atan2(velocity.y, velocity.x)) + particleAngleOffset
+        var particleAngle : float
+        if normVel.y > 0:
+            particleAngle = 180
+        elif normVel.y < 0:
+            particleAngle = 0
+        elif normVel.x > 0:
+            particleAngle = 270
+        elif normVel.x < 0:
+            particleAngle = 90
+        
+        ringParticles.angle_max = particleAngle
+        ringParticles.angle_min = particleAngle
+    else:
+        ringParticles.emitting = false
--- Objects/Player/Controller2D.gd
+++ Objects/Player/Controller2D.gd
@@ -14,7 +14,7 @@ func _physics_process(delta):
 	var direction :Vector2
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
-	print(rotation_degrees)
+	# print(rotation_degrees)
 	if direction:
 		direction.normalized()
 		velocity = direction * SPEED

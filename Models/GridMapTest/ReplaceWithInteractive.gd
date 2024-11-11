3a43ca578cb48053b515c6fdf7fc6d6707e47c95
Kimomo <d.s.zampathas@gmail.com>
2024-11-11 12:00:54 -0800

    GridMapUpdate (Replace Items with interactable ones)

---
+++ Models/GridMapTest/ReplaceWithInteractive.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+@onready var gridMap : GridMap = get_parent()
+@export var itemToReplace : int
+@export var item : PackedScene
+
+
+func _ready() -> void:
+	var itms = gridMap.get_used_cells_by_item(itemToReplace)
+	for itm in itms:
+		var replace = item.instantiate()
+		replace.name = str(itm)
+		add_child(replace)
+		(replace as Node3D).global_basis = gridMap.get_cell_item_basis(itm)
+		(replace as Node3D).global_position = gridMap.map_to_local(itm) - Vector3(0,.5,0)
+		print(gridMap.get_cell_item(itm))
+		gridMap.set_cell_item(itm,0)
+		

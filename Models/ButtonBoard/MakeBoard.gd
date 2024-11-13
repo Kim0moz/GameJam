38d91f3fdff478cb166db368fb405c23068b11d8
Kimomo <d.s.zampathas@gmail.com>
2024-11-13 14:45:20 -0800

    ButtonGrid

--- Models/ButtonBoard/MakeBoard.gd
+++ Models/ButtonBoard/MakeBoard.gd
@@ -1,12 +1,33 @@
+@tool
 extends Node3D
 
 @export var button : PackedScene
-@onready var buttons = $"Buttons"
+@export var buttons : Node3D
 @export var buttonData : Array[Dictionary] = [{"transform": Vector3(0,0,0), "rotations": [11,15,3,07]}]
 @export var grid : GridMap
+@export var columns : int = 4
+
+@export var yOffset : float = 0:
+	set(val):
+		yOffset = val
+		build_grid()
+@export var zOffset : float = 0:
+	set(val):
+		zOffset = val
+		build_grid()
+@export var zPadding : float = 0:
+	set(val):
+		zPadding = val
+		build_grid()
+@export var yPadding : float = 0:
+	set(val):
+		yPadding = val
+		build_grid()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#_add_data(16)
+	build_grid()
 	pass # Replace with function body.
 
 
@@ -14,14 +35,25 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _add_data(amount: int):
+	buttonData.clear()
+	for i in range(amount):
+		buttonData.append({"transform": Vector3(0,0,0), "rotations": [11,15,3,07]})
+
 func build_grid():
 	var index = 0
-	for butts in buttons.get_children():
-		butts.free()
+	if buttons:
+		for butts in buttons.get_children():
+			butts.free()
 	for butt in buttonData:
-		var tmp : Node3D = button.instantiate()
-		add_child(tmp)
-		tmp.name = str(buttons.get_child_count())
-		tmp.position = Vector3(0-2,0,0)
+		if butt.transform != Vector3.ZERO:
+			var tmp : pipeLever = button.instantiate()
+			buttons.add_child(tmp,true)
+			tmp.name = str(buttons.get_child_count())
+			tmp.set_owner(self)
+			tmp.grid = grid
+			tmp.rots = butt.rotations 
+			tmp.PipeIndex = butt.transform
+			tmp.position = Vector3(.1,yOffset+floor(index/columns)/yPadding,zOffset+(index%columns)/zPadding)
 		index+=1
 		
--- Objects/ToggleableItems/PipeLever.gd
+++ Objects/ToggleableItems/PipeLever.gd
@@ -1,17 +1,17 @@
 extends Node3D
+class_name pipeLever
 
 @onready var lever  = $'Lever'
 @export var grid : GridMap
 @export var itemIndex : int
 @export var PipeIndex : Vector3
 var index : int = 0
-@export var Positions : Array[int]
+@export var rots : Array
 
 func _ready() -> void:
 	lever.Activated.connect(Toggle)
 
 func Toggle():
-	grid.set_cell_item(PipeIndex,itemIndex,Positions[index])
-	index = (index+1)%Positions.size()
-	
+	grid.set_cell_item(PipeIndex,itemIndex,rots[index])
+	index = (index+1)%rots.size()
 	

abd1bdb1855f021eeaecf8e1d854ddb8825e7271
Kimomo <d.s.zampathas@gmail.com>
2024-11-20 21:15:30 -0800

    UpdatedPipes

---
+++ Environments/pipe_direction_support.gd
@@ -0,0 +1,39 @@
+@tool
+extends Node3D
+class_name PipeSupport
+
+@export var outlets : Array = [[Vector3i(0,0,0),Vector3i(0,0,0)]]
+@onready var Grid : GridMap = self.get_parent()
+
+func activeDirections(pipe : int, cellRotation):
+	var list = []
+	for outlet in outlets[pipe]:
+		list.append(rotateToOrientation(outlet,cellRotation))
+	return list
+
+func rotateToOrientation(pos : Vector3, cellRotation):
+	var oris = [
+		Vector3(0,0,1),
+		Vector3(0,1,0),
+		Vector3(0,0,-1),
+		Vector3(0,-1,0),
+		Vector3(-1,0,0),
+		Vector3(1,0,0)
+		]
+	var flooredVal : float = floor(cellRotation/4)
+	var modVal : float = (cellRotation%4)
+	var firstRot = ((flooredVal)/4)*360
+	var secondRot = (modVal/4)*360
+
+	var FirstRot = pos.rotated(oris[flooredVal],deg_to_rad(secondRot))
+	var FinalRot 
+	if cellRotation >= 16 and cellRotation < 20:
+		FinalRot = FirstRot.rotated(Vector3(0,1,0),deg_to_rad(90))
+	elif cellRotation >= 20:
+		FinalRot = FirstRot.rotated(Vector3(0,1,0),deg_to_rad(90))
+		FinalRot = FinalRot.rotated(Vector3(1,0,0),deg_to_rad(180))
+	else:
+		FinalRot = FirstRot.rotated(Vector3(1,0,0),deg_to_rad(firstRot))
+	return Vector3i(FinalRot)
+	
+	
--- Environments/pipe_item.gd
+++ Environments/pipe_item.gd
@@ -3,16 +3,47 @@ extends Node3D
 
 
 @export var Grid : GridMap
-@export var CurrentPos : Vector3i = Vector3i(-2,-1,-1)
-@export var Next : bool
-@export var Previous : bool
+@export var CurrentPos : Vector3i = Vector3i(-2,-1,-1):
+	set(val):
+		CurrentPos = val
+		moveToGridLocation(val)
+@export var Next : bool :
+	set(val):
+		nextPos()
+@export var Previous : bool :
+	set(val):
+		prePos()
+@export var pipeDirection : PipeSupport
+
+@onready var pipeMesh = $Item
+
+
+var currentCell
+var ActiveDirections
+var lastDirection = Vector3(0,0,0)
 
 func _ready() -> void:
 	moveToGridLocation(CurrentPos)
 
 func nextPos():
-	return false
+	moveToGridLocation(CurrentPos+ActiveDirections[1])
+	lastDirection = ActiveDirections[1]
+	
+func prePos():
+	moveToGridLocation(CurrentPos+ActiveDirections[0])
+	lastDirection = ActiveDirections[0]
+
+func updatePipeDetails():
+	var currentCell = Grid.get_cell_item(CurrentPos)
+	ActiveDirections = pipeDirection.activeDirections(currentCell,Grid.get_cell_item_orientation(CurrentPos))
+
+func calculateNextDirection():
+	pass
 
 func moveToGridLocation(location : Vector3i):
+	if CurrentPos == location:
+		return
+	CurrentPos = location
 	self.global_position = Grid.local_to_map(location)
-	self.global_basis =  Grid.get_cell_item_basis(location)
+	pipeMesh.global_basis =  Grid.get_cell_item_basis(location)
+	updatePipeDetails()

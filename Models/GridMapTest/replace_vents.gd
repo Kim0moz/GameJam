2ff9ed3f6c376019ba34b5b5bd40c4770fa1fcbf
Kimomo <d.s.zampathas@gmail.com>
2024-11-29 13:30:31 -0800

    FixedVents to be edible in editor

--- Models/GridMapTest/replace_vents.gd
+++ Models/GridMapTest/replace_vents.gd
@@ -1,45 +1,31 @@
 @tool
 extends "res://Models/GridMapTest/ReplaceWithInteractive.gd"
 
-@export var data : Dictionary
-@export var interactive : Array[Dictionary]
+@export var interactive : Array[Vector3i]
 @export var player : Node3D
+@export var replaceVents : bool:
+	set(val):
+		updateVents()
 @export var getNodes : bool : 
 	set(val):
 		getInteractive()
 
 func _ready() -> void:
-	if Engine.is_editor_hint() == false:
-		for itm in interactive:
-			if itm.Exit != Vector3i.ZERO:
-				var replace = item.instantiate()
-				replace.player = player
-				replace.name = str(itm.transform)
-				add_child(replace)
-				itm.Self = replace.get_path()
-				(replace as Node3D).global_basis = gridMap.get_cell_item_basis(itm.transform)
-				(replace as Node3D).global_position = gridMap.map_to_local(itm.transform) + gridMap.global_position
-				gridMap.set_cell_item(itm.transform,-1)
-		for itm in interactive:
-			if itm.Exit != Vector3i.ZERO:
-				get_node(itm.Self).Data.Exit = getExitPoint(itm.Exit)
+	pass
 		
 func getInteractive():
 	var itms = gridMap.get_used_cells_by_item(itemToReplace)
+	interactive.clear()
 	for itm in itms:
-		print(itm)
-		if checkNodes(itm) == false:
-			interactive.append(data.duplicate())
-			interactive[interactive.size()-1].transform = itm
+		interactive.append(itm)
 
-func checkNodes(pos):
-	for item in interactive:
-		if item.transform == pos:
-			return true
-	return false
-
-func getExitPoint(t : Vector3i):
+func updateVents():
 	for itm in interactive:
-		if t == itm.transform:
-			print(itm.Exit)
-			return itm.Self
+		var replace = item.instantiate()
+		replace.player = player
+		add_child(replace)
+		replace.owner = get_tree().edited_scene_root
+		replace.name = str(itm)
+		(replace as Node3D).global_basis = gridMap.get_cell_item_basis(itm)
+		(replace as Node3D).global_position = gridMap.map_to_local(itm) + gridMap.global_position
+		gridMap.set_cell_item(itm,-1)
--- Models/GridMapTest/vent_wall.gd
+++ Models/GridMapTest/vent_wall.gd
@@ -1,14 +1,13 @@
 extends InteractableItem
 
-@export var Data : Dictionary = {
-	"Exit" : "",
-}
 @export var player : Node3D
-@export var	ExitPoint : Node3D
+@export var ExitPoint : Node3D
+@export var locked : bool = false
 
 
 func move():
-	player.basis = get_node(Data.Exit).basis
-	player.global_position = get_node(Data.Exit).global_position
-	print("Moving from, ", self, "Moving to, ", Data.Exit)
-	player.position.y += 1
+	if ExitPoint and locked == false:
+		player.basis = ExitPoint.basis
+		player.global_position = ExitPoint.global_position
+		print("Moving from, ", self, "Moving to, ", ExitPoint)
+		player.position.y += 1

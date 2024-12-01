df180fefc966614b8cdd1ed5b76582de5c82996e
Kimomo <d.s.zampathas@gmail.com>
2024-11-30 16:44:15 -0800

    AddedRankingSystem

--- UI/PopulateEmployees.gd
+++ UI/PopulateEmployees.gd
@@ -3,7 +3,12 @@ extends VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var children = get_children()
+	var hs = GlobalVariables.highScores
+	for i in range(children.size()):
+		var item = children[i]
+		var hsi = hs[i]
+		(item as EmployeePanel).updatePanel(hsi.Score,hsi.Employee,hsi.Icon,str(int(item.name)+1))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
